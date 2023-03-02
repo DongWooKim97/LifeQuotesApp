8ee0608f495a4a19f885d437e1458cc9c4fcae16
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 17:03:30 +0900

    feat: 처음부터 다시 만드는 명령앱 1단계 코드 작성(Scanner로 입력)

---
+++ src/main/java/com/lifequotes_main/Application.java
@@ -0,0 +1,13 @@
+package com.lifequotes_main;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        String command = sc.nextLine();
+
+    }
+}
