c07740cc6bfb496477fd4b706f4182526768d556
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 21:20:14 +0900

    feat: 목록을 보여주기 전 사전멘트 출력 메서드 생성

--- src/main/java/com/lifequotes_main/view/OutputView.java
+++ src/main/java/com/lifequotes_main/view/OutputView.java
@@ -13,4 +13,9 @@ public class OutputView {
         System.out.println(id + "번 명언이 등록되었습니다.");
     }
 
+    public void printShowListComment() {
+        System.out.println("번호 / 작가 / 명언\n" +
+                "----------------------");
+    }
+
 }
