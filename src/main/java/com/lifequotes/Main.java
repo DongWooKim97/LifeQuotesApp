05a0ac632bae568407e4933dabfe1f9a7c402905
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 09:22:40 +0900

    feat: 명언앱 2단계 코드 작성

--- src/main/java/com/lifequotes/Main.java
+++ src/main/java/com/lifequotes/Main.java
@@ -1,9 +1,21 @@
 package com.lifequotes;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        String operation = sc.nextLine();
+        
+        System.out.print("명언 : ");
+        String quotes = sc.nextLine();
+
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
         System.out.println("명언) 종료");
 
     }
