1805a3c53e8fe435d55de5a67e7feb451b4badaf
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 20:55:54 +0900

    feat: 명언에 대한 정보를 기억하는 클래스 QuotesController 생성

---
+++ src/main/java/com/lifequotes_main/domain/QuotesList.java
@@ -0,0 +1,14 @@
+package com.lifequotes_main.domain;
+
+public class QuotesList {
+    private int id;
+    private String quote;
+    private String author;
+
+    public QuotesList(int id, String quote, String author) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+    }
+
+}
