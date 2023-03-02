69c425a026dc09e9073cc994c8950da2f34e3350
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 17:36:37 +0900

    feat: 명언, 저자를 입력받고 스캐너의 객체를 받을 수 있는 메서드 생성

--- src/main/java/com/lifequotes_main/view/InputView.java
+++ src/main/java/com/lifequotes_main/view/InputView.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
 
     public String readCommand() {
         System.out.print("명령) ");
         String command = sc.nextLine();
 
         return command;
+    }
+
+    public String readQuote() {
+        System.out.print("명언 : ");
+        String quote = sc.nextLine();
+
+        return quote;
+    }
+
+    public String readAuthor() {
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
+        return author;
+    }
+
+    public Scanner getSc() {
+        return sc;
     }
 }
