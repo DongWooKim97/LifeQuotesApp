8ea12c128304708658729d3dc5ecbf662f085de7
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 20:56:40 +0900

    feat: 명언 정보를 담을 리스트 생성

--- src/main/java/com/lifequotes_main/controller/QuotesController.java
+++ src/main/java/com/lifequotes_main/controller/QuotesController.java
@@ -1,7 +1,11 @@
 package com.lifequotes_main.controller;
 
+import com.lifequotes_main.domain.QuotesList;
 import com.lifequotes_main.view.InputView;
 import com.lifequotes_main.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuotesController {
     private final InputView inputView = new InputView();
@@ -12,7 +16,9 @@ public class QuotesController {
     }
 
     public void run() {
+        List<QuotesList> list = new ArrayList<>();
 
+        int id = 1;
 
         while (true) {
             String command = inputView.readCommand();
@@ -21,9 +27,17 @@ public class QuotesController {
                 inputView.getSc().close();
                 outputView.printEndComment();
                 break;
+
             } else if (command.equals("등록")) {
                 String quote = inputView.readQuote();
                 String author = inputView.readAuthor();
+
+                QuotesList ql = new QuotesList(id, quote, author);
+
+                list.add(ql);
+                outputView.printRegisterSuccess(id);
+                id++;
+
             }
         }
 
