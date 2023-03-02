02329e21af56cf45395a6ecc5d88ba4ea88c682e
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 09:31:30 +0900

    feat: 명언 클래스 작성 및 반복 명령을 위한 반복문 작성

--- src/main/java/com/lifequotes/Main.java
+++ src/main/java/com/lifequotes/Main.java
@@ -6,17 +6,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String operation = sc.nextLine();
+
+        int id = 1;
+        while(true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.print("명령) ");
+            String operation = sc.nextLine();
+
+            System.out.print("명언 : ");
+            String quote = sc.nextLine();
+
+            System.out.print("작가 : ");
+            String author = sc.nextLine();
+
+            Quotes q = new Quotes(id, operation, quote, author);
+            id++;
+
+            break;
+        }
+
+        sc.close();
         
-        System.out.print("명언 : ");
-        String quotes = sc.nextLine();
+        System.out.println("명령) 종료");
 
-        System.out.print("작가 : ");
-        String author = sc.nextLine();
+    }
+}
 
-        System.out.println("명언) 종료");
+class Quotes {
+    private int id;
+    private String operation;
+    private String quote;
+    private String author;
 
+    public Quotes(int id, String operation, String quote, String author) {
+        this.id = id;
+        this.operation = operation;
+        this.quote = quote;
+        this.author = author;
+
+        System.out.println(this.id+"번 명언이 "+this.operation+"되었습니다.");
     }
 }
