194381ea596e6760d6c6a4218563d30ea4ad134d
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 17:09:28 +0900

    refactor: App과 Controller 분리 후 실행 클래스 생성(QuotesController)

--- src/main/java/com/lifequotes_main/Application.java
+++ src/main/java/com/lifequotes_main/Application.java
@@ -1,13 +1,11 @@
 package com.lifequotes_main;
 
-import java.util.Scanner;
+import com.lifequotes_main.controller.QuotesController;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String command = sc.nextLine();
+
+        new QuotesController().run();
 
     }
 }
