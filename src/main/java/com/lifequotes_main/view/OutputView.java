9c3ff792122065ceeaa436b7c3a8b7eb75dd4a68
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 17:36:58 +0900

    feat: 명령 앱 시작과 끝에 해당하는 문구를 출력하는 메서드 생성

--- src/main/java/com/lifequotes_main/view/OutputView.java
+++ src/main/java/com/lifequotes_main/view/OutputView.java
@@ -1,5 +1,12 @@
 package com.lifequotes_main.view;
 
 public class OutputView {
+    public void printStartComment() {
+        System.out.println("== 명령 앱 시작 ==");
+    }
+
+    public void printEndComment() {
+        System.out.println("== 명령 앱 종료 ==");
+    }
 
 }
