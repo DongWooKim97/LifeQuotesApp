c6ed0f946d4ce115f07ea53cd4e1eab3603d189a
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 11:36:03 +0900

    feat: 명언 7단계 코드 작성 완료

--- src/main/java/com/lifequotes/Main.java
+++ src/main/java/com/lifequotes/Main.java
@@ -15,7 +15,7 @@ class App {
     Scanner sc;
 
     public App(Scanner sc) {
-        this. sc = sc;
+        this.sc = sc;
     }
 
     public void init() {
@@ -23,58 +23,78 @@ class App {
 
         List<Quotes> quotesList = new ArrayList<>();
 
+//        Map<Integer, Quotes> quotesMap = new HashMap<>();
+
         int id = 1;
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String operation = sc.nextLine().trim();
 
-            if(operation.equals("등록")) {
+            if (operation.equals("등록")) {
                 System.out.print("명언 : ");
                 String quote = sc.nextLine().trim();
 
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
 
-                quotesList.add(new Quotes(id, operation, quote, author));
-                id++;
-            }
+                Quotes q = new Quotes(id, operation, quote, author);
+                quotesList.add(q);
+//                quotesMap.put(id, q);
 
-            if (operation.equals("목록")) {
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                id++;
+            } else if (operation.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언\n" +
                         "----------------------");
-                for (int i = quotesList.size()-1; i >=0; i--) {
-                    quotesList.get(i).showList();
+                for (int i = quotesList.size() - 1; i >= 0; i--) {
+                    Quotes tmpQ = quotesList.get(i);
+                    System.out.println(tmpQ.getId() + " / " + tmpQ.getQuote() + " / " + tmpQ.getAuthor());
+
                 }
                 continue;
-            }
-
-            if(operation.equals("종료")) {
+            } else if (operation.equals("종료")) {
                 sc.close();
                 break;
-            }
+            } else if (operation.contains("삭제?id=")) {
+                int removeId = Integer.parseInt(String.valueOf(operation.charAt(operation.length() - 1)));
 
+                try {
+                    quotesList.remove(removeId - 1);
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", removeId);
+                } catch (Exception e) {
+                    System.out.printf("%d번 명언이 존재하지 않습니다.\n", removeId);
+//                }
+
+
+                }
+
+            }
         }
     }
-}
 
-class Quotes {
-    private int id;
-    private String operation;
-    private String quote;
-    private String author;
+    class Quotes {
+        private int id;
+        private String operation;
+        private String quote;
+        private String author;
 
-    public Quotes(int id, String operation, String quote, String author) {
-        this.id = id;
-        this.operation = operation;
-        this.quote = quote;
-        this.author = author;
+        public Quotes(int id, String operation, String quote, String author) {
+            this.id = id;
+            this.operation = operation;
+            this.quote = quote;
+            this.author = author;
+        }
 
-        System.out.println(this.id+"번 명언이 "+this.operation+"되었습니다.");
+        public int getId() {
+            return id;
+        }
+
+        public String getQuote() {
+            return quote;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
     }
-
-    public void showList() {
-        System.out.println(this.id +" / "+ this.author + " / " + this.quote);
-    }
-
-
 }
