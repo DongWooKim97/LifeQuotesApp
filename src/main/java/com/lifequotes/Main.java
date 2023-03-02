02592a64e40b478321a034b97604e97e507ba46c
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 09:18:12 +0900

    feat: 명언앱 1단계 코드 작성

--- src/main/java/com/lifequotes/Main.java
+++ src/main/java/com/lifequotes/Main.java
@@ -2,6 +2,9 @@ package com.lifequotes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("== 명언 앱 ==");
+        System.out.println("명언) 종료");
+
     }
 }
