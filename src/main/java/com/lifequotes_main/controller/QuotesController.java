ddd57a6b1ff80dd252a8b2aa8691da80a6511f42
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 17:37:55 +0900

    feat: 반복문을 통해 입력값에 따른 로직을 수행하는 코드 작성

--- src/main/java/com/lifequotes_main/controller/QuotesController.java
+++ src/main/java/com/lifequotes_main/controller/QuotesController.java
@@ -1,13 +1,32 @@
 package com.lifequotes_main.controller;
 
 import com.lifequotes_main.view.InputView;
+import com.lifequotes_main.view.OutputView;
 
 public class QuotesController {
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
+    public QuotesController() {
+        outputView.printStartComment();
+    }
 
     public void run() {
-        System.out.println("== 명언 앱 ==");
-        String command = inputView.readCommand();
+
+
+        while (true) {
+            String command = inputView.readCommand();
+
+            if (command.equals("종료")) {
+                inputView.getSc().close();
+                outputView.printEndComment();
+                break;
+            } else if (command.equals("등록")) {
+                String quote = inputView.readQuote();
+                String author = inputView.readAuthor();
+            }
+        }
+
+
     }
 }
