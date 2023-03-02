ffd8a65bdfa7bb33e98bc5ba0c6f3b1df8063840
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-03-02 22:13:58 +0900

    feat: 명언 클래스에 문구와 저자 setter메서드 작성

--- src/main/java/com/lifequotes_main/domain/Quotes.java
+++ src/main/java/com/lifequotes_main/domain/Quotes.java
@@ -19,7 +19,15 @@ public class Quotes {
         return quote;
     }
 
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
